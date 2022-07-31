a109e31d512bb96c3521dccb5cbb9d106b7ff72b
MrSumit04 <104508553+MrSumit04@users.noreply.github.com>
2022-07-31 18:41:55 +0530

    Update alive.py

--- Himawari/modules/alive.py
+++ Himawari/modules/alive.py
@@ -25,11 +25,11 @@ SOFTWARE.
 from telethon import events, Button, custom
 from Himawari.events import register
 from Himawari import telethn as tbot
-HIMAWARI = "https://telegra.ph/file/9dfcdab5244a61b323210.jpg"
+HIMAWARI = "https://telegra.ph/file/d41c5e5d4fda3a00be038.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   STB = event.sender.first_name
-  STB = "**I m Himawari** \n\n" + "**I'm Working Properly**\n\n"
+  STB = "**I m Yor Forger** \n\n" + "**I'm Working Properly**\n\n"
   STB += "**Python Version : 3.9.7**\n\n"
   STB += "**python-Telegram-Bot : 13.7**\n\n"
   BUTTON = [[Button.url("Support", "https://t.me/{SUPPORT_CHAT}"), Button.url("Updates", "https://t.me/{UPDATES_CHANNEL}")]]
